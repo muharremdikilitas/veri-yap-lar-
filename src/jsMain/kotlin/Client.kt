7e787d349fdecaa70d66d7b142fd230c9d2f58b7
muharremdikilitas <muharrem_273@hotmail.com>
2025-03-10 21:12:57 +0300

    first commit

---
+++ src/jsMain/kotlin/Client.kt
@@ -0,0 +1,20 @@
+import kotlinx.browser.document
+import react.create
+import react.dom.client.createRoot
+
+fun main() {
+    val x
+    var editableVar = "Java"
+    editableVar = "Kotlin"
+    var name : String
+    name = "ali"
+
+    val firstname = "Kerem"
+    val lastName = "Aktürk"
+
+
+    //operator overloading
+
+    val fullName = firstname + lastName
+    println(fullName)
+}
